aac42435f5ade62741f535034354e6acdadeefe2
cedney12 <cade@intercotradingco.com>
2025-03-26 10:37:58 -0500

    React Query should now detect url change

--- src/hooks/useQueryData.jsx
+++ src/hooks/useQueryData.jsx
@@ -1,34 +1,30 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 /**
  * Custom Hook that uses React-Query to fetch and cache data, using
- * the URL as the query key
+ * the URL as the query key.
  *
  * @param {string} endpoint
  * @param {object} [options]
  * @param {number} [options.staleTime] Time by which the data goes stale and must be recalled, in ms
  * @param {number} [options.retry] If `true`, retries infinitely. If `false`, never retries. If a number, retries that many times
- * @import {UseQueryResult} from '@tanstack/react-query'
- * @returns {UseQueryResult}
+ * @returns {object} { data, error, isLoading, refetch, setUrl }
  * 
  * @category Hooks
  * @module useQueryData
  */
-export const useQueryData = (
-    endpoint,
-    options = {}
-) => {
-
+export const useQueryData = (endpoint, options = {}) => {
     const [url, setUrl] = useState(endpoint)
 
-    const trimUrlForKey = () => {
+    const trimUrlForKey = (url) => {
         if (!url) return ""
-        let newUrl = new URL(url, import.meta.VITE_API_BASE_URL)
+        const newUrl = new URL(url, import.meta.env.VITE_API_BASE_URL)
         return newUrl.pathname + newUrl.search
     }
+
+    const queryKey = ["data", trimUrlForKey(url)]
 
     const queryFn = async () => {
         const { data } = await axios.get(url)
@@ -37,12 +33,12 @@ export const useQueryData = (
 
     return {
         ...useQuery({
-            queryKey: ["data", trimUrlForKey(url)],
+            queryKey,
             queryFn,
             staleTime: options.staleTime ?? 60000,
-            enabled: !!url, // disables call if URL is falsey
-            retry: 1
+            enabled: !!url, // disables call if URL is falsy
+            retry: options.retry ?? 1,
         }),
-        setUrl
+        setUrl,
     }
 }
